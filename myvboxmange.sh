7e41c55e49966cc7bb0266159c1db062b41d17de
Sreenu Sasubilli <sasubillis@gmail.com>
2014-03-27 12:34:53 +0200

    Update myvboxmange.sh

--- myvboxmange.sh
+++ myvboxmange.sh
@@ -4,6 +4,7 @@ rm /etc/udev/rules.d/70-persistent-net.rules
 
 
 # clone the existing VM (with out snapshots)
+# go to VirtualBoxVms directory
 
 vboxmanage clonevm <Name of VM> --name <New Name> --register
 
