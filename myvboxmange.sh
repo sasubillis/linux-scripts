9dba14d6a7265a2f4e76003e3842cb27cdc3b57d
Sreenu Sasubilli <sasubillis@gmail.com>
2014-03-27 12:10:28 +0200

    Update myvboxmange.sh

--- myvboxmange.sh
+++ myvboxmange.sh
@@ -1,3 +1,14 @@
+# First delete net persistent rules
+
+rm /etc/udev/rules.d/70-persistent-net.rules
+
+
 # clone the existing VM (with out snapshots)
 
 vboxmanage clonevm <Name of VM> --name <New Name> --register
+
+
+VBoxManage startvm Clone -type headless
+
+
+VBoxManage guestproperty get Clone "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk '{split($0,a," "); print a[2]}'
