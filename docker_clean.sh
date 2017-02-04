09538ea964e70ab2dfbac88ef303fc675f4e4851
Sreenu Sasubilli <sasubillis@gmail.com>
2017-02-04 21:32:30 +0200

    Update docker_clean.sh

--- docker_clean.sh
+++ docker_clean.sh
@@ -6,13 +6,13 @@ docker rmi -f `docker images -aq`
 docker rm $(docker ps --filter status=exited --quiet)
 
 # remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+#docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
 
 # remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+#docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 
 # remove unused volumes:
-find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
-  docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
-) | xargs -r rm -fr
+#find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+# docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
+#) | xargs -r rm -fr
 }
